83f0f8fb9574c0de2d282fa5d5f596f24f2a25f4
yash.sehgal@sarvika.com <yash.sehgal@sarvika.com>
2024-11-23 16:50:14 +0530

    new_website

--- stone/stone/spiders/stone_scraper.py
+++ stone/stone/spiders/stone_scraper.py
@@ -1,6 +1,7 @@
 import time
 from typing import Iterable, Any
 
+import requests
 import scrapy
 from scrapy.http import Response
 from typing import List
@@ -13,6 +14,9 @@ class StoneSpider(scrapy.Spider):
         "https://royalestones.co.uk/categories/floor-tiles.html",
         "https://royalestones.co.uk/categories/wall-tiles.html",
         "https://royalestones.co.uk/categories/bathroom-tiles-1.html",
+        "https://royalestones.co.uk/categories/bathroom-furiture.html",
+        "https://royalestones.co.uk/categories/acoustic-slat-wood-panels.html",
+        "https://royalestones.co.uk/categories/paving-essentials.html",
     ]
 
     def parse(self, response: Response, **kwargs: Any) -> Any:
@@ -41,20 +45,40 @@ class StoneScraper2(scrapy.Spider):
     name = "stonemart"
     start_urls = [
         "https://www.thestonemart.co.uk/collections/indian-sandstone-paving",
+        "https://www.thestonemart.co.uk/collections/limestone-paving",
+        "https://www.thestonemart.co.uk/collections/granite-paving",
+        "https://www.thestonemart.co.uk/collections/porcelain-paving",
+        "https://www.thestonemart.co.uk/collections/slate-paving",
+        "https://www.thestonemart.co.uk/collections/driveway-paving",
+        "https://www.thestonemart.co.uk/collections/paving-accessories",
     ]
 
     def parse(self, response: Response, **kwargs: Any) -> Any:
+        if response.status != 200:
+            return
         for items in response.css("div.collection"):
-            image = items.xpath(".//li/div/div/div/div/a/img/@src")
-            title = items.xpath(".//li/div/div/div/div[1]/a/@data-product-title")
-            price = items.xpath(".//li/div/div/div/div[1]/div/div/span[" "1]/text()")
+            image = items.xpath(".//li/div/div/div/div/a/img/@src").getall()
+            title = items.xpath(
+                ".//li/div/div/div/div[" "1]/a/@data-product-title"
+            ).getall()
+            price = items.xpath(
+                ".//li/div/div/div/div[1]/div/div/span[" "1]/text()"
+            ).getall()
             discount_price = items.xpath(
                 ".//li/div/div/div/div[" "1]/div/div/span[2]/text()"
-            )
-            for no, i in image:
+            ).getall()
+            for no, i in enumerate(image):
                 yield {
                     "title": title[no],
                     "image": f"https:{image[no]}",
                     "price": price[no],
                     "discount_price": discount_price[no],
                 }
+            next_page = f"https://www.stonemart.co.uk{response.css("div#CollectionProductGrid div div nav div button::attr(data-href)").get()}"
+            data = requests.get(next_page)
+            print(data.content)
+            print(next_page)
+            response.follow(next_page, callback=self.parse)
+
+    def parse_next(self, response: Response, **kwargs: Any) -> Any:
+        pass
